371975588a7098546922db299edb652735ee43f2
somni <evgengeodakyan@gmail.com>
2026-02-03 12:45:54 -0800

    textnode created, gitignore added

---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum, auto
+
+
+class TextType(Enum):
+    PLAIN = auto()
+    BOLD = auto()
+    ITALIC = auto()
+    CODE = auto()
+    LINK = auto()
+    IMAGE = auto()
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+
+        return (
+            self.text == other.text,
+            self.text_type == other.text_type,
+            self.url == other.url,
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
