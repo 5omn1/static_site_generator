5f6b5ecbf5c95a9939b71fd4fc6363df8acac2e2
somni <evgengeodakyan@gmail.com>
2026-02-05 10:06:03 -0800

    index.css updated

--- src/copy_dir.py
+++ src/copy_dir.py
@@ -2,12 +2,14 @@ import os
 import shutil
 
 
-def copy_dir_recursive(src_dir, dest_dir, log=True):
+def copy_dir_recursive(src_dir: str, dest_dir: str) -> None:
     if not os.path.isdir(src_dir):
-        raise ValueError(
-            f"Source directory does not exist or is not directory: {src_dir}"
-        )
+        raise ValueError(f"Source directory does not exist: {src_dir}")
+
+    # Create dest dir if it doesn't exist (this is why public/ not existing is OK)
     os.makedirs(dest_dir, exist_ok=True)
+
+    # Delete everything inside dest dir (clean build)
     for name in os.listdir(dest_dir):
         path = os.path.join(dest_dir, name)
         if os.path.isdir(path):
@@ -15,16 +17,18 @@ def copy_dir_recursive(src_dir, dest_dir, log=True):
         else:
             os.remove(path)
 
-    def _copy(current_src, current_dest):
-        os.makedirs(current_dest, exist_ok=True)
-        for name in os.listdir(current_src):
-            src_path = os.path.join(current_src, name)
-            dest_path = os.path.join(current_dest, name)
+    # Recursively copy
+    def _copy(cur_src: str, cur_dest: str) -> None:
+        os.makedirs(cur_dest, exist_ok=True)
+
+        for name in os.listdir(cur_src):
+            src_path = os.path.join(cur_src, name)
+            dest_path = os.path.join(cur_dest, name)
 
             if os.path.isdir(src_path):
                 _copy(src_path, dest_path)
             else:
-                shutil.copy(src_path, dest_path)
-                if log:
-                    print(f"Copied {src_path} -> {dest_path}")
-        _copy(src_dir, dest_dir)
+                shutil.copy2(src_path, dest_path)
+                print(f"Copied: {src_path} -> {dest_path}")
+
+    _copy(src_dir, dest_dir)
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
         "This is some anchor text", TextType.LINK, "https://www.boot.dev"
     )
     print(text_node)
-    copy_dir_recursive("static", "public", log=True)
+    copy_dir_recursive("static", "public")
 
 
 if __name__ == "__main__":
